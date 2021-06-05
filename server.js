e4a8e662da5c6df89569492c2bc5648094ba376c
Adrian <adriansiv97@hotmail.com>
2021-06-05 18:26:02 +0200

    First Part

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
+
+
 
 const welcomeMessage = {
   id: 0,
@@ -20,6 +25,37 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+app.post("/messages", (req, res) => {
+  const lastMessage = messages[messages.length - 1]
+  let infoMessage = {
+    id: lastMessage.id + 1,
+    from: req.body.from,
+    text: req.body.text
+  }
+  messages.push(infoMessage);
+  console.log(messages);
+  res.send(infoMessage);
+});
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+})
+
+app.delete("/messages/:id", (req, res) => {
+  const ID = req.params.id
+  // const messageID = messages.map(item => (item.id));
+  // const messageToDelete = messageID.find(idToDelete => idToDelete === ID);
+   const index = messages.findIndex(message => message.id == ID);
+ 
+  if (index > -1) {
+    messages.splice(index, 1);
+    res.status(200).send({ success: true });
+  }
+  else {
+    res.status(404).send();
+  };
+  
+})
 app.listen(3000, () => {
    console.log("Listening on port 3000")
   });
